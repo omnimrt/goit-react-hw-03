90bb0a9ab40c66c32da662b70e8c9bdc690b3891
Martin Myhalyna <martinmyhalyna@gmail.com>
2024-03-22 19:57:59 +0100

    03_react_homework

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import ContactForm from "./assets/components/ContactForm/ContactForm";
+
+function App() {
+  return (
+    <>
+      <h1>Phonebook</h1>
+      <ContactForm />;
+    </>
+  );
+}
+
+export default App;
